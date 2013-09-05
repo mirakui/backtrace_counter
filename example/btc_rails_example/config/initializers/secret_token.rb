2864545a8b2eaa90cd90eb9146a80b736ebe95b7
Issei Naruta <naruta@cookpad.com>
2013-09-05 20:48:00 +0900

    Add a rails example

---
+++ example/btc_rails_example/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BtcRailsExample::Application.config.secret_key_base = 'd4bf94d420bad9d696e2bb6f3433e86a21a919e4507ae4ecfdef3dfbc6472b5de22398be525a8d81731f6ab97f84c82f7fe4860ed42f3b7406f8a59898fa6692'
---
+++ example/btc_rails_example/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BtcRailsExample::Application.config.session_store :cookie_store, key: '_btc_rails_example_session'
